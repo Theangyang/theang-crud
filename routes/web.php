0d170f4c26e7caa5e1d9a9800718187bfb2c123d
Theangyang <altheaangelcapecenio000@gmail.com>
2026-05-28 19:45:55 +0800

    Fix web route names

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ Route::get('/', function () {
     return redirect()->route('notes.index');
 });
 
-Route::resource('notes', NoteController::class);
+Route::resource('notes', NoteController::class)->names([
+    'index' => 'notes.index',
+    'create' => 'notes.create',
+    'store' => 'notes.store',
+    'show' => 'notes.show',
+    'edit' => 'notes.edit',
+    'update' => 'notes.update',
+    'destroy' => 'notes.destroy',
+]);
