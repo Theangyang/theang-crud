c7cf6cec169c3cc5a2fcc1b65ac203d3837d7d8e
Theangyang <altheaangelcapecenio000@gmail.com>
2026-05-28 18:14:19 +0800

    Fix duplicate route names

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NoteController as ApiNoteController;
 
-Route::apiResource('notes', ApiNoteController::class);
+Route::apiResource('notes', ApiNoteController::class)->names([
+    'index' => 'api.notes.index',
+    'store' => 'api.notes.store',
+    'show' => 'api.notes.show',
+    'update' => 'api.notes.update',
+    'destroy' => 'api.notes.destroy',
+]);
